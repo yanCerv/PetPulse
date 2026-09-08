1c7ab312308c9777b4e7ff9b5281893e2f3bf590
Yan Cervantes <acervantes@ia.com.mx>
2026-09-08 23:21:05 +0200

    First Commit

---
+++ PetPulse/PetPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetPulseApp.swift
+//  PetPulse
+//
+//  Created by Yan Cervantes  on 08/09/26.
+//
+
+import SwiftUI
+
+@main
+struct PetPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
